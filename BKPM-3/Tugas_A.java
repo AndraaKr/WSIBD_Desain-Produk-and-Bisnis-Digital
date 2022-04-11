cb16b27f0690310ea0efb839a30718cfaa01606c
AndraaKr <102611292+AndraaKr@users.noreply.github.com>
2022-04-11 10:27:15 +0700

    Add files via upload

---
+++ BKPM-3/Tugas_A.java
@@ -0,0 +1,13 @@
+package BKPM3;
+import java.util.Arrays;
+public class Tugas_A {
+public static void main(String[] args) {
+        // Array satu dimensi bagian nama, nama saya Muhammad Andra Kusuma Ramadhan
+        String[] teks =  {"Muhammad" , "Andra" , "Kusuma" , "Ramadhan"};
+        for (int i = 0; i < teks.length; i++) {
+            System.out.print(" " +teks[i]);
+        }
+        System.out.print(" ");
+    }
+    
+}
